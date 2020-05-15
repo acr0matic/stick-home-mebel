41900a25576e78d67cf4229c6ceb8160216f0158
Nikita Mateyuk <work.acr0matic@gmail.com>
2020-05-16 01:28:35 +0300

    Сверстана форма обратной связи

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,6 @@
+let callbackButton = document.querySelector(".callback-action");
+let callbackForm = document.querySelector(".form-callback");
 
+callbackButton.addEventListener("click", () => {
+  callbackForm.classList.toggle("form-callback--visible");
+});
