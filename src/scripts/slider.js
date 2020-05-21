41a26c073b638c7a99214b429378828488fe8895
Nikita Mateyuk <work.acr0matic@gmail.com>
2020-05-21 12:28:21 +0300

    Доработка галерей

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,22 +1,40 @@
 /* global Swiper */
 
-let gallerySlider = new Swiper ('.gallery-slider', {
+let gallerySliderContainer = document.querySelector(".gallery-slider");
 
-  spaceBetween: 30,
+if (gallerySliderContainer) {
+  let gallerySlider = new Swiper(gallerySliderContainer, {
+    spaceBetween: 30,
 
-  pagination: {
-    el: '.gallery-slider__pagination',
-    clickable: true,
-  },
+    pagination: {
+      el: ".gallery-slider__pagination",
+      clickable: true,
+    },
 
-  navigation: {
-    nextEl: '.gallery-slider__next',
-    prevEl: '.gallery-slider__prev',
-  },
+    navigation: {
+      nextEl: ".gallery-slider__next",
+      prevEl: ".gallery-slider__prev",
+    },
 
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-})
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+  });
 
-gallerySlider.init();
+  gallerySlider.init();
+  CheckContollers(gallerySlider);
+}
+
+function CheckContollers(slider) {
+  let slideCount = slider.slides.length;
+
+  let leftArrow = document.querySelector(".swiper-button-prev");
+  let rightArrow = document.querySelector(".swiper-button-next");
+  let pagination = document.querySelector(".swiper-pagination");
+
+  if (slideCount == 1) {
+    leftArrow.style.display = "none";
+    rightArrow.style.display = "none";
+    pagination.style.display = "none";
+  }
+}
