84c328f53eaa06ab58738f15d04b97924a26840d
Nikita Mateyuk <work.acr0matic@gmail.com>
2020-05-21 18:05:44 +0300

    Сделан адаптив главной страницы

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,11 +1,21 @@
 /* global MicroModal */
 
-let callbackButton = document.querySelector(".callback-action");
+let callbackButtons = document.querySelectorAll(".callback-action");
 let callbackForm = document.querySelector(".form-callback");
 
-callbackButton.addEventListener("click", () => {
-  callbackForm.classList.toggle("form-callback--visible");
-});
+let hamburger = document.querySelector(".hamburger");
+let mobileMenu = document.querySelector(".mobile-menu");
+
+for (const button of callbackButtons) {
+  button.addEventListener("click", () => {
+    callbackForm.classList.toggle("form-callback--visible");
+
+    if (mobileMenu.classList.contains("mobile-menu--open")) {
+      mobileMenu.classList.toggle("mobile-menu--open");
+      hamburger.classList.toggle("is-active");
+    }
+  });
+}
 
 window.addEventListener("click", (e) => {
   let target = e.target;
@@ -17,5 +27,10 @@ let modalListeners = document.querySelectorAll("[data-material]");
 for (const modal of modalListeners) {
   modal.addEventListener("click", () => {
     MicroModal.show("materialModal");
-  })
+  });
 }
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("is-active");
+  mobileMenu.classList.toggle("mobile-menu--open");
+});
